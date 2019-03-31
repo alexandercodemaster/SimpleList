b0bca8b43749c1899bf024a0cc49c3eefe30cef4
Alexander Chelmaev <chelmaevalex@rambler.ru>
2019-03-31 17:58:18 +0300

    Список языков программирования готов.

--- app/src/main/java/com/alexander/simplelist/MainActivity.java
+++ app/src/main/java/com/alexander/simplelist/MainActivity.java
@@ -2,12 +2,26 @@ package com.alexander.simplelist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView plList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        plList = findViewById(R.id.tv_pl_names);
+
+        String[] names = {"Java", "Python", "Ruby", "JavaScript", "Groovy"};
+
+        plList.setText("");
+
+        for (String name : names) {
+            plList.append(name + "\n");
+        }
     }
 }
